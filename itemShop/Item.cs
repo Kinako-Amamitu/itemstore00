7edb644186a98fb5ce751c8d1c0a48e68cc8420a
S-JOBI23-060\student <231122@st.yoshida-g.ac.jp>
2024-08-30 09:40:50 +0900

    プロジェクト作成

---
+++ itemShop/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itemShop
+{
+    internal class Item
+    {
+        public Item(int v1, string v2, int v3)
+        {
+            new Item(1, "ひのきのぼう", 80);
+            new Item(2, "こんぼう", 300);
+            new Item(3, "銅の剣", 500);
+        }
+
+    }
+}
---
+++ itemShop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itemShop
+{
+    internal class Program
+    {
+        //所持金
+        int money=1000;
+
+        //アイテム
+        List<Item> itemList = new List<Item>() {
+    new Item(1, "ひのきのぼう", 80),
+    new Item(2, "こんぼう", 300),
+    new Item(3,"銅の剣",500)
+};
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("いらっしゃいませ。");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("何をお探しですか？");
+
+            //キー入力
+            Console.ReadLine();
+        }
+    }
+}
